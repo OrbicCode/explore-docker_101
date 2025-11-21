9590de7ac3f109091de68302a48f121e606d822a
James O'Kane <orbiccode@gmail.com>
2025-11-21 15:02:09 +0000

    CSV_URL change

--- backend/seedDb.js
+++ backend/seedDb.js
@@ -1,7 +1,8 @@
 import pool from './db/dbConfig';
 import { parse } from 'csv-parse';
 
-const CSV_URL = 'https://gist.github.com/armgilles/194bcff35001e7eb53a2a8b441e8b2c6.js';
+const CSV_URL =
+  'https://gist.githubusercontent.com/armgilles/194bcff35001e7eb53a2a8b441e8b2c6/raw/92200bc0a673d5ce2110aaad4544ed6c4010f687/pokemon.csv';
 const parser = parse({
   delimiter: ',',
   columns: true,
