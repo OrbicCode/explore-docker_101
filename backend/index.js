775907cd04b42b9a029a499089dd0414bc09c3a6
James O'Kane <orbiccode@gmail.com>
2025-11-24 15:52:02 +0000

    installed vite and cors

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import { pokemonRoutes } from './routes.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.use('/pokemon', pokemonRoutes);
 
---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <div>
+        <ul></ul>
+      </div>
+    </main>
+  );
+}
+
+export default App;
