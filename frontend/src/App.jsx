442ec2b4e86cc8ff9c35a31e63826d6b8c88b27f
James O'Kane <orbiccode@gmail.com>
2025-11-24 16:05:14 +0000

    added handleClick function to get pokemon from backend and display in a list

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,27 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [pokemon, setPokemon] = useState([]);
+
+  async function handleClick() {
+    try {
+      const response = await fetch('http://localhost:3000/pokemon/get-ten');
+      const data = await response.json();
+      setPokemon(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const pokemonList = pokemon && pokemon.map((pokemon) => <li key={pokemon.id}>{pokemon.name}</li>);
   return (
     <main>
       <div>
-        <ul></ul>
+        <button onClick={handleClick}>get Pokemon</button>
+      </div>
+      <div>
+        <ul>{pokemonList}</ul>
       </div>
     </main>
   );
